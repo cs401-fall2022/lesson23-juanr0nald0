f5e74248c3cbf6f87359f0509ead7a0e9f713901
Juan <juan.ronaldo.garcia@gmail.com>
2022-11-16 00:12:57 -0700

    Updated variable names for clarity

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Welcome to Blogster!'));
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public', 'index.html')); });
+
+app.post('/message', (req, res) => {
+
+    res.set({ 'Content-Type': 'text/plain; charset=utf-8' });
+
+    let title = req.body.entryTitle;
+    let body = req.body.entryBody;
+    let output = `${entryTitle}: ${entryBody}`;
+
+    res.send(output);
+});
 
 app.listen(3000, () => console.log('Blogster is up and running... listening on port 3000'));
