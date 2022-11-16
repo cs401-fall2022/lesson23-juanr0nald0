b26c3ae532a31f6eada4d8073be09d47165e8dc0
Juan <juan.ronaldo.garcia@gmail.com>
2022-11-16 00:24:04 -0700

    main.js progress. Form, close app.

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
+const db = new sqlite3.Database('data/test.db');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+// Respond with the homepage
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public', 'index.html')); });
 
+app.get('entries')
+
+// Send form data
 app.post('/message', (req, res) => {
 
     res.set({ 'Content-Type': 'text/plain; charset=utf-8' });
@@ -19,3 +26,19 @@ app.post('/message', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Blogster is up and running... listening on port 3000'));
+
+// Close the app
+process.on('SIGINT', () => {
+
+    db.close((err) => {
+
+        console.log('Blogster is shutting down');
+
+        if (err) {
+            console.error(err.message);
+        }
+          console.log('Securing your blog entries.');
+        });
+
+    server.close();
+});
