36872e55db79eaa363d94afc0daba0c78e6123a2
Juan <juan.ronaldo.garcia@gmail.com>
2022-11-17 17:24:41 -0700

    Got a working database!!!!

--- data/entries.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-DROP TABLE IF EXISTS entries;
-
-CREATE TABLE entries (id INTEGER PRIMARY KEY, title TEXT, body TEXT, currentDate TEXT);
-INSERT INTO entries(title, body, currentDate) VALUES('Oh Em Gee!', 'I got a new job!', DATE('now'));
-INSERT INTO entries(title, body, currentDate) VALUES('Flat tire', 'I cannot believe I got a flat tire!', DATE('now'));
-INSERT INTO entries(title, body, currentDate) VALUES('Oh Em Gee 2', 'I got a new NEW job!', DATE('now'));
-INSERT INTO entries(title, body, currentDate) VALUES('You-know-who', 'I ran into you-know-who at the gas station... more to follow', DATE('now'));
-COMMIT;
--- main.js
+++ main.js
@@ -1,28 +1,38 @@
 const express = require('express');
-const path = require('path');
+const path = require('path');       // Is this still necessary?
 const sqlite3 = require('sqlite3').verbose();
 const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const db = new sqlite3.Database('data/blogsterEntries.db', (err) => {
-    if (err) {
-        return console.error(err.message);
-    }
-    console.log('Connected to the blogsterEntries SQLite3 database.')
-});
+// var db = new sqlite3.Database('/data/blogsterEntries.db', sqlite3.OPEN_READWRITE, (err) => {   // Trying ':memory:', instead of the db file because of error code: 'SQLITE_NOTADB'
+var db = new sqlite3.Database(path.resolve(__dirname,'data/blogsterEntries.sqlite')
+);
 
 // Express module code... >>>>>>>>>>>>>>>>>>>>>> MAY NEED
 // app.use(express.static('public'));
 // app.use(express.urlencoded({ extended: true }));
 
 // Fire up the database
-// db.run
+db.run('CREATE TABLE IF NOT EXISTS entries(id INTEGER PRIMARY KEY, title TEXT, body TEXT, currentDate TEXT)');
 
 // Respond with the homepage
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'public', 'index.html')); });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.get('entries')
+// Create a blog entry
+app.get('/', (req, res) => {
+    db.serialize(() => {
+        db.run('INSERT INTO entries(id, title, body, currentDate) VALUES(?,?,?,?)', [req.params.id, req.params.title, req.params.body, req.params.currentDate], function(err) {
+            if (err) {
+                return console.log(err.message);
+            }
+            console.log("New blog entry added successfully!");
+            res.send("New blog entry has been added to the database");
+        });
+    });
+});
 
 // Send form data
 app.post('/message', (req, res) => {
@@ -47,11 +57,10 @@ process.on('SIGINT', () => {
 
         if (err) {
             console.error(err.message);
-        }
-          console.log('Error in closing database... trying to secure your blog entries.');
-        });
-    
-    server.close();
+            console.log('Error in closing database... trying to secure your blog entries.');
+        }          
+    });
     console.log('Blogster has closed successfully');
+    server.close();
     process.exit();
 });
